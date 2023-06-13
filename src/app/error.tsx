c786f31159caf05a2cf60c04b06926fcc68c0911
coke-code <wissbell@163.com>
2023-06-13 09:09:27 +0800

    feat: add route

---
+++ src/app/error.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main>
+      <h1>error page</h1>
+    </main>
+  )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main>
+      <h1>404 Not Found</h1>
+      <p>This is a custom 404 page.</p>
+    </main>
+  )
+}

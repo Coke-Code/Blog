8eca48a5927d9e64e19f0a063015efbadb92bde6
Coke-Code <1148606675@qq.com>
2023-06-17 10:27:45 +0800

    feat:sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+
+const getAllPosts = () => {
+	const dir = path.resolve(__dirname, "./content/blog/");
+	let allBlogPosts = [];
+	try {
+		const files = fs.readdirSync(dir);
+		allBlogPosts = files
+			.filter((v) => v.endsWith(".mdx"))
+			.map((v) => v.replace(".mdx", ""));
+	} catch (err) {
+		console.log(err);
+	}
+	return allBlogPosts;
+};
 module.exports = {
 	siteUrl: process.env.SITE_URL || "https://www.wissbell.com",
 	generateRobotsTxt: true, // (optional)
-	additionalPaths: async (config) => [
-		await config.transform(config, "/additional-page"),
-	],
+	additionalPaths: async (config) => {
+		const result = ["/", "/about", "/contact}"].map((v) =>
+			config.transform(config, v),
+		);
+		const allBlogPosts = getAllPosts();
+		const blogs = allBlogPosts.map((post) =>
+			config.transform(config, `blog/${post}`),
+		);
+		const routes = await Promise.all(result.concat(blogs));
+		return routes;
+	},
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: "*",
+				allow: "/",
+			},
+		],
+		additionalSitemaps: [
+			"https://www.wissbell.com/sitemap.xml",
+			"https://www.wissbell.com/sitemap-0.xml",
+		],
+	},
 };
